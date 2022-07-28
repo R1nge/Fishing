c834615bb9b4f3e06687414d88a85e7b10d7c2cb
R1nge <r1ngegamedev@gmail.com>
2022-07-28 19:38:03 +0300

    added fishing rod save manager

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using FishingRod;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    [SerializeField] private FishingRodSo so;
+
+    private void Awake()
+    {
+        if (File.Exists(Application.persistentDataPath + "/Rod.json"))
+        {
+            LoadFromJson();
+        }
+        else
+        {
+            so.data = new RodData
+            {
+                rodSprite = "global[Fish1]",
+                hookSprite = "FishingHook",
+                biteSprite = "",
+                maxWeight = 15,
+                maxLength = 25,
+                verticalSpeed = 10,
+                horizontalSpeed = 3000
+            };
+        }
+
+        so.OnSpriteChanged += SaveToJson;
+    }
+
+    private void SaveToJson() =>
+        File.WriteAllText(Application.persistentDataPath + "/Rod.json", JsonUtility.ToJson(so.data));
+
+    private void LoadFromJson()
+    {
+        string fileContents = File.ReadAllText(Application.persistentDataPath + "/Rod.json");
+        so.data = JsonUtility.FromJson<RodData>(fileContents);
+    }
+
+    private void OnDestroy() => so.OnSpriteChanged -= SaveToJson;
+}
